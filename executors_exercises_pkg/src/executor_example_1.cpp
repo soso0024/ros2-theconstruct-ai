aba77300ace3850aab117334d604914513f96038
soso0024 <131151398+soso0024@users.noreply.github.com>
2024-04-22 19:50:02 +0200

    add ex4 program

---
+++ executors_exercises_pkg/src/executor_example_1.cpp
@@ -0,0 +1,20 @@
+#include "rclcpp/executors/single_threaded_executor.hpp"
+#include "rclcpp/logging.hpp"
+#include <rclcpp/rclcpp.hpp>
+
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+
+  // Instantiate a node
+  rclcpp::Node::SharedPtr node =
+      rclcpp::Node::make_shared("executor_example_1_node");
+
+  RCLCPP_INFO(node->get_logger(), "Bacon pancakes, making bacon pancakes");
+
+  rclcpp::executors::SingleThreadedExecutor executor;
+  executor.add_node(node);
+  executor.spin();
+
+  rclcpp::shutdown();
+  return 0;
+}
