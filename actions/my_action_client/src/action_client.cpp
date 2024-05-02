f70013ff7b87fa403675436ee359dd6807ff890d
soso0024 <131151398+soso0024@users.noreply.github.com>
2024-05-02 21:04:51 +0200

    add actions part

--- actions/my_action_client/src/action_client.cpp
+++ actions/my_action_client/src/action_client.cpp
@@ -18,7 +18,7 @@ public:
     this->client_ptr_ = rclcpp_action::create_client<Move>(
         this->get_node_base_interface(), this->get_node_graph_interface(),
         this->get_node_logging_interface(),
-        this->get_node_waitables_interface(), "move_robot_as");
+        this->get_node_waitables_interface(), "move_robot_as_2");
 
     this->timer_ =
         this->create_wall_timer(std::chrono::milliseconds(500),
